e40eca6bc68fe17788b10c4d7b93dbc0a3e96d73
benclark82 <beneclark@gmail.com>
2021-02-23 22:49:44 -0600

    Different injected types with qualifiers

--- src/main/java/guru/springframework/sfgdi/services/GreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/GreetingService.java
@@ -1,9 +1,13 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by jt on 12/26/19.
  */
 public interface GreetingService {
 
+    @Autowired
     String sayGreeting();
 }
